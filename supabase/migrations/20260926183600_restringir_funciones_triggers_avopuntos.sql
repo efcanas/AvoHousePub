c4b4c50df9ad7346152e1b540f4d47edd8c96dac
efcanas <ef.canas@icloud.com>
2026-09-26 13:35:28 -0500

    Restringir funciones internas de triggers AvoPuntos

---
+++ supabase/migrations/20260926183600_restringir_funciones_triggers_avopuntos.sql
@@ -0,0 +1,7 @@
+revoke all on function public.avopuntos_ensure_account() from public;
+revoke all on function public.avopuntos_ensure_account() from anon;
+revoke all on function public.avopuntos_ensure_account() from authenticated;
+
+revoke all on function public.avopuntos_requeue_changed_receipt() from public;
+revoke all on function public.avopuntos_requeue_changed_receipt() from anon;
+revoke all on function public.avopuntos_requeue_changed_receipt() from authenticated;
