194be0420c6d3f7f4170f77823c4f19bcd5f5db9
efcanas <ef.canas@icloud.com>
2026-09-26 13:50:45 -0500

    Corregir permisos internos del motor AvoPuntos

---
+++ supabase/migrations/20260926185100_corregir_permisos_helper_avopuntos.sql
@@ -0,0 +1,2 @@
+grant execute on function public.avopuntos_receipt_input_hash(public.loyverse_receipts) to service_role;
+grant execute on function public.avopuntos_requeue_changed_receipt() to service_role;
